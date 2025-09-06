4ce79a9e86c69a47d8a2d144074f46688c8acd35
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-06 11:53:24 +0530

    add jinja2

--- flask/flask/jinja.py
+++ flask/flask/jinja.py
@@ -3,6 +3,12 @@ from flask import Flask,request,render_template
 app = Flask(__name__)
 
 # Variable rules
+# jinja2 template
+'''
+    {{}} expression to print output in html
+    {% %} control flow,loops
+    {# #} comments
+'''
 @app.route('/success/<int:score>')
 def success(score):
     # return "the person score is, "+ str(score)
@@ -12,7 +18,26 @@ def success(score):
     else:
         res = "fail"
 
-    return render_template('result.html', result=res)                
+    return render_template('result.html', result=res) 
+
+
+@app.route('/successResult/<int:score>')
+def successResult(score):
+    # return "the person score is, "+ str(score)
+    res = ""
+    if score > 50:
+        res = "pass"
+    else:
+        res = "fail"
+
+
+    exp = {
+        'score':score,
+        'result':res
+    }
+    return render_template('result.html', exp=exp) 
+
+
 @app.route('/route_name')
 def method_name():
     pass
