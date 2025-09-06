10acf121d380f351ea226e2682bea9809a0582b7
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-07 00:03:56 +0530

    add json

---
+++ flask/flask/api.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def api_data():
+    query = request.args.get('query')
+    data = {'results': [
+        {'id': 1, 'name': 'John'},
+        {'id': 2, 'name': 'Steve'},
+        {'id': 3, 'name': 'Bill'},
+    ]}
+    if query:
+        data['results'] = [item for item in data['results'] if query in item['name']]
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
