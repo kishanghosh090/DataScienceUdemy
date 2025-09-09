8f2f8387034266946ae2de2c81bcc2dc11b7eba6
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-09 23:51:03 +0530

    add streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,3 +1,25 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+
+### title of the aplication
+
+st.title("hello from chai")
+
+## display simple text
+
+st.write("hello from chai")
+
+
+### create data frame
+
+df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+# st.dataframe(df)
+st.write(df)
+
+## create a line chart 
+
+st.line_chart(df)
+
+
---
+++ streamlit/widgets.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title("My first app")
+st.write("Here's our first attempt at using data to create a table:")
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+st.write(df)
+
+chart_data = pd.DataFrame(
+    np.random.randn(50, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+name=st.text_input("enter your name: ")
+
+if name:
+    st.write("hello "+name)
