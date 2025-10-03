2d45f6b3f6b4c82d1f38556f679f4867f50bc2b1
kishanghosh090 <kishanghosh090@gmail.com>
2025-10-03 23:40:59 +0530

    remove GENAI python

--- chaiAurPython/02_datatypes/chapter_2.py
+++
@@ -1,8 +0,0 @@
-spice_mix = set()
-
-print(f"initial spice mix id : {id(spice_mix)}")
-
-spice_mix.add("ginger")
-spice_mix.add("cardamom")
-
-print(f"second initial mix id: {id(spice_mix)}")
--- chaiAurPython/oops/01.py
+++
@@ -1,9 +0,0 @@
-class Chai:
-    pass
-
-print(type(Chai))
-
-
-gingerTea = Chai()
-print(type(gingerTea))
-print(gingerTea is Chai)
